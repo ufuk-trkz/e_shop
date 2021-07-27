c62cdd6ebf628bd7d0c08981452da042f3813ca0
ufuk-trkz <ufuk_trkz@hotmail.de>
2021-07-27 11:55:38 +0200

    Add registration VC UI components

--- lib/Widgets/customTextField.dart
+++ lib/Widgets/customTextField.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget
-{
+class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final IconData data;
   final String hintText;
   bool isObsecure = true;
-
 
 
   CustomTextField(
@@ -14,12 +12,27 @@ class CustomTextField extends StatelessWidget
       ) : super(key: key);
 
 
-
   @override
-  Widget build(BuildContext context)
-  {
-    return Container
-    (
+  Widget build(BuildContext context) {
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.all(10),
+      child: TextFormField(
+        controller: controller,
+        obscureText: isObsecure,
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: Icon(data, color: Theme.of(context).primaryColor),
+          focusColor: Theme.of(context).primaryColor,
+          hintText: hintText,
+        ),
+      ),
     );
   }
 }
